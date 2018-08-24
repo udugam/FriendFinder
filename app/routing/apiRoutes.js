0cc5d65d27434a595a02c903f8bb1c41317bee28
udugam <ryan.udugam@gmail.com>
2018-08-23 22:00:41 -0400

    added survey questions, successfully writing friend objects to file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 
     //This route will add a new friend to the friends.js file while also running the compatability function
     app.post('/api/friends', function(req,res) {
-        fs.appendFile('./app/data/friends.js', JSON.stringify(req.body)+'\r', function(err) {
+        fs.appendFile('./app/data/friends.js', JSON.stringify(req.body)+',\r', function(err) {
             if (err) throw err
             console.log('Write successful!')
             res.redirect('/survey')
